12f5246b8a7b5ffb11f0f6ff7246ac02fa85d957
allinux <alextrusting1@gmail.com>
2025-03-28 10:12:59 -0300

    adicionando anotações

--- ponteiro1.c
+++ ponteiro1.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 int main(void){
+    //x inicia com o valor 10
+
     int x = 10;
-    int *ponteiro;
-    ponteiro = &x;
+    int *ponteiro; //específicando que a minha váriavel é um ponteiro
+    
+    // ponteiro recebee o endereço da váriavel x 
+
+    ponteiro = &x; //ponteiro arquivando endereço 
+
+    // y recebe o valor 20 
 
     int y = 20;
-    *ponteiro = y;
+
+    // o endereço do ponteiro recebe o valor de y 
+
+    *ponteiro = y; //endereço do ponteiro arquivando valor 
+
+    //acessar valor do ponteiro - sem *
+    //acessar endereço do ponteiro - com *
+    //acessar endereço da váriavel - com &
+    //acessar conteúdo da váriavel - sem &
 
     printf("%d %d\n", x, y);
 
