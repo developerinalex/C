10db65f5a2d0ea9fcb3b2f8df9e5dddbf64cd820
allinux <alextrusting1@gmail.com>
2025-04-01 08:40:02 -0300

    algo estranho ta acontecendo aqui

--- ponteiro8.c
+++ ponteiro8.c
@@ -2,9 +2,12 @@
 
 int main(void){
     int vetor[3] = {1, 2, 3};
+
     int *pVet = vetor;  //em vetores não precisamos identificar o &
 
-    printf("%d\n", *pVet);
+    *(pVet + 1) = 10;
+
+    printf("%d\n", vetor[1]);
 
     getchar();
     return 0;
