a3185bf7a57624b4f4fa88a6b426de3b657dfb44
allinux <alextrusting1@gmail.com>
2025-04-03 07:57:05 -0300

    deu trabalho mas eu entendi

--- ponteiro6.c
+++ ponteiro6.c
@@ -4,7 +4,8 @@ int main(void){
 
     struct lista{
         int valor;
-        struct lista *proximo;
+        struct lista *proximo;  //eu tava com uma dúvida do pq esse ponteiro simplesmente não criar um loop obrigando ele a armazenar outra struct dentro dele até que eu 
+        //que um ponteiro só armazena ENDEREÇO, ISSO DEIXA DE ARAMZENAR OUTRA STRUCT A PARTIR DO MOMENTO QUE TRANSFORMA-SE EM PONTEIRO!!!
     };
 
     struct lista m1, m2, m3;
@@ -14,7 +15,7 @@ int main(void){
     m2.valor = 20;
     m3.valor = 30;
 
-    m1.proximo = &m2;
+    m1.proximo = &m2; //a gente dar um endereço pra próximo
     m2.proximo = &m3;
     m3.proximo = (struct lista *)0;
 
