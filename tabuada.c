30cf072588abce8e4960d49a9d419c894419e7a8
allinux <alextrusting1@gmail.com>
2025-04-04 20:35:34 -0300

    algumas alterações de precisão e interação

--- tabuada.c
+++ tabuada.c
@@ -9,6 +9,11 @@ int main(void){
     printf("Digite um número de 1 a 10 para exibirmos a sua tabuada.\n");
     scanf("%d", &numero);
 
+    while(numero < 1 || numero > 10){
+        printf("Erro, digite um número válido.\n");
+        scanf("%d", &numero);
+    }
+
     contas = tabuada(numero);
 
     getchar();
@@ -17,10 +22,25 @@ int main(void){
 
 int tabuada(int n){
     int resultado, multiplicador = 0;
+    char input;
+    char *verificador = &input;
+
 
     for(multiplicador = 0; multiplicador <= 10; multiplicador++){
         resultado = n * multiplicador;
         printf("%d x %d = %d\n", n, multiplicador, resultado);
     }
+
+    printf("Deseja continuar? (s/n): ");
+    scanf(" %c", verificador);
+
+    if(*verificador == 's'){
+        printf("Digite outro número: ");
+        scanf("%d", &n);
+        tabuada(n);
+    }
+    else{
+        printf("Fim do programa.\n");
+    }
     return 0;
-}
+}   
