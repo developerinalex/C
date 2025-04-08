f1254a5b0e8682186247d9d0cdea89cc46e9124e
allinux <alextrusting1@gmail.com>
2025-04-08 11:15:33 -0300

    caixa bancario - finalizado(apenas a função de saque)

--- caixa.c
+++ caixa.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
+int cedulas(int valor);
+
 int main(void){
 
     int saque;
 
+    printf("Dísponivel apenas cédulas de 100, 50, 20, 10 e 5. Por favor, digite um valor que atenda a observção.\n");
+
     printf("Digite o valor de saque: ");
     scanf("%d", &saque);
 
-    if(saque < 5){
-        printf("Valor insuficiente, digite uma quantia válida: ");
-        scanf("%d", &saque);
-    }
+    cedulas(saque);
+
+    getchar();
+    return 0;
 }
 
+int cedulas(int valor){
+    int notas[5] = {100, 50, 20, 10, 5};
+    int i = 0;
+    
+    for(i = 0; i < 5; i++){
+        int qtd = valor / notas[i];
+        if(qtd > 0){
+            printf("%d notas de R$%d foram usadas.\n", qtd, notas[i]);
+        }
+        valor %= notas[i];
+        if(valor == 0){
+            break;
+        }
+    }
+
+    return valor;
+}
