459573e3031e175e39538d0c8e4e5feaee880658
allinux <alextrusting1@gmail.com>
2025-04-15 20:23:36 -0300

    só um tapa no código

--- lacos/conversorTemperatura/temperatura.c
+++ lacos/conversorTemperatura/temperatura.c
@@ -5,3 +5,12 @@ void menu(void){
     puts("bem-vindo ao conversor de temperatura. O que você deseja fazer?");
     printf("1. Celsius para Fahrenheit.\n2. Fahrenheit para Celsius.\n3. Sair.\n");
 }
+
+void celsiusF(int celsius){
+    int c = celsius, grauF = 0;
+
+    grauF = (c * 9/5) + 32;
+
+    printf("%d°F.\n", grauF);
+    menu();
+}
