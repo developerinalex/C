b7caa14b5a6052c64dbb1a6898a5e8fce412cf2d
allinux <alextrusting1@gmail.com>
2025-04-15 20:16:28 -0300

    conversor de temperatura

--- lacos/conversorTemperatura/temperatura.c
+++ lacos/conversorTemperatura/temperatura.c
@@ -2,5 +2,6 @@
 #include "temperatura.h"
 
 void menu(void){
-    
+    puts("bem-vindo ao conversor de temperatura. O que você deseja fazer?");
+    printf("1. Celsius para Fahrenheit.\n2. Fahrenheit para Celsius.\n3. Sair.\n");
 }
