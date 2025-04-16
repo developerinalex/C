358a785c6e58e7ff9e2fa5169a40cf4a632c96aa
allinux <alextrusting1@gmail.com>
2025-04-16 08:41:39 -0300

    finalizado

--- lacos/conversorTemperatura/temperatura.c
+++ lacos/conversorTemperatura/temperatura.c
@@ -9,9 +9,9 @@ void menu(void){
     do{
         printf("1. Celsius para Fahrenheit.\n2. Fahrenheit para Celsius.\n3. Sair.\n");
 
-        while(scanf("%d", &seletor) != 1){
+        while(scanf("%d", &seletor) != 1){ //faz com que seja impossivel digitar algo diferente de um int
             puts("Opção inválida, digite um número inteiro.");
-            limparBuffer();
+            limparBuffer(); //limpa o buffer para o scanf funcionar corretamente 
         }
 
         switch(seletor){
@@ -29,10 +29,10 @@ void menu(void){
                 printf("Opção inválida, digite algo válido.\n");
                 break;
         }
-    }while(seletor != 3);
+    }while(seletor != 3); //garante que a função seja repetida até que o usuário deseje encerrar o programa
 }
 
-void celsiusF(void){
+void celsiusF(void){ //converte celsius para fahrenheit
     int c = 0, grauF = 0;
 
     printf("Digite a temperatura em Celsius: ");
@@ -45,7 +45,7 @@ void celsiusF(void){
     limparBuffer();
 }
 
-void fahrenheitC(void){
+void fahrenheitC(void){ //converte fahrenheit para celsius
     int f = 0, grauC = 0;
 
     printf("Digite a temperatura em Fahrenheit: ");
@@ -55,10 +55,10 @@ void fahrenheitC(void){
 
     printf("%d°C.\n", grauC);
 
-    limparBuffer();
+    limparBuffer(); 
 }
 
-void limparBuffer(void){
+void limparBuffer(void){ //limpa o buffer até o fim da linha
     int lixo;
     while((lixo = getchar()) != '\n' && lixo != EOF);
 }
